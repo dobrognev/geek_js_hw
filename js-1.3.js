9ef227b33868e333debd0ef706448f6cdd893af6
dobrognev <kroto911@gmail.com>
2020-09-25 16:42:26 +0300

    Commit

--- js-1.3.js
+++ js-1.3.js
@@ -2,22 +2,33 @@ function mrRandom() {
     return parseInt(Math.round(Math.random() * 500))
 }
 
-function primeNumbers(n) { // TODO Сюда будет вставляться рандомное число
-    let mrNum = n - (n - 1)
-    console.log(`Вычисляем все простые числа до ${n}`)
-    while (n > mrNum) {
-        let numRoot = Math.ceil(Math.sqrt(n));
-
-        while (numRoot > 1) {
-            if (mrNum % numRoot === 0) {
-                console.log(`${mrNum} - составное число`)
-            }
-            numRoot--;
-        }
-
+function primeNumber(n) {
+    if (n < 1) {
+        console.log(`Число должно быть больше единиы`)
     }
-    console.log(numRoot)
+    let numRoot = Math.ceil(Math.sqrt(n));
+    while (numRoot > 1) {
+        if (n % numRoot === 0) {
+            return false
+        }
+        numRoot--;
+    }
+    return true;
 }
+// primeNumber(mrRandom());
 
+function primeNumRange(n) {
 
-primeNumbers(mrRandom());
+    let mrNum = n - (n - 1);
+    let arrNum = [];
+    document.write(`Вычисляем все простые числа в промежутке от 0 до ${n} : <br \/>`);
+    while (n >= mrNum) {
+        if (primeNumber(mrNum) == true) {
+            console.log(`Число ${mrNum} - простое`);
+            arrNum.push('  ' + mrNum + '  ');
+        }
+        mrNum++;
+    }
+    document.write(arrNum);
+}
+primeNumRange(mrRandom());
