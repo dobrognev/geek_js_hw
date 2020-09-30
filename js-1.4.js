c61f4d6ace4e56b4acf073ef705d078f2346c1b6
dobrognev <kroto911@gmail.com>
2020-09-30 20:49:42 +0300

    Opa

--- js-1.4.js
+++ js-1.4.js
@@ -1,32 +1,34 @@
 // Что-то я увлекся немного
 // TODOO Первая задачка
-function mrNum_999() {
-    return Math.round(Math.random() * 999)
+'use strict';
+
+function mrNum999() {
+    return Math.round(Math.random() * 999);
 }
 
-let numberBox = {}
+let numberBox = {};
 
 function numParser(num) {
     num += "";
     if (num > 999) {
-        console.log("Сгенерите число меньше чем 999")
+        console.log("Сгенерите число меньше чем 999");
         return {};
     }
     let numParse = num.split("");
     console.log(numParse);
     if (numParse.length == 1) {
         return numberBox.hundreds = "0", numberBox.dozens = "0",
-            numberBox.units = numParse[0]
+            numberBox.units = numParse[0];
     } else if (numParse.length == 2) {
         return numberBox.hundreds = "0", numberBox.dozens = numParse[0],
-            numberBox.units = numParse[1]
+            numberBox.units = numParse[1];
     } else {
         return numberBox.hundreds = numParse[0], numberBox.dozens = numParse[1],
-            numberBox.units = numParse[2]
+            numberBox.units = numParse[2];
     }
 }
 
-numParser(mrNum_999());
+numParser(mrNum999());
 console.log(numberBox);
 
 
@@ -36,3 +38,10 @@ console.log(numberBox);
 
 // TODOO Третья задачка
 //! Кто хочет стать миллионером
+
+let user = "";
+alert(prompt(user = "Введи свое имя, смертный !", "Dobrognev"));
+alert(`Добро пожаловать на игру, ${user}`);
+alert(`Сейчас вам будет задано 5 вопросов, каждый последующий из них добавляет не только сложности, но и денег`);
+
+let sumCash;
